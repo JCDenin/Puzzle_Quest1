16ded8e7fd11a3eccba06f8d8b7f3894ae954d9b
JCDenin <vityadm@gmail.com>
2023-06-29 17:30:57 +0300

    Initial commit

---
+++ app/src/main/java/com/teamspirt/puzzlequest/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.teamspirt.puzzlequest
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.teamspirt.puzzlequest.ui.theme.PuzzleQuestTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PuzzleQuestTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Abaldui")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+            text = "Hello $name!",
+            modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PuzzleQuestTheme {
+        Greeting("Guest")
+    }
+}
