ccc9d0bf1a3a640fc4cc2bd64d21f4c493ed3dfd
JCDenin <vityadm@gmail.com>
2023-07-04 17:45:30 +0300

    Updating files

--- app/src/main/java/com/teamspirt/puzzlequest/MainActivity.kt
+++ app/src/main/java/com/teamspirt/puzzlequest/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.teamspirt.puzzlequest
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.ImageView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,7 +15,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.teamspirt.puzzlequest.ui.theme.PuzzleQuestTheme
 
-class MainActivity : ComponentActivity() {
+/*class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -24,8 +27,17 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
+}*/
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
+        val imageView: ImageView = findViewById(R.id.imageView3)
+        imageView.setImageResource(R.drawable.photo) // Replace "your_image" with the actual image resource name
+    }
+}
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
